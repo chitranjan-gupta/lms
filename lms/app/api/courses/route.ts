4c951882966886849146bbbba8fe18ee228f5e25
Chitranjan Gupta <37315561+chitranjan-gupta@users.noreply.github.com>
2024-05-10 19:19:48 +0530

    Update route.ts
    removing isTeacher

--- lms/app/api/courses/route.ts
+++ lms/app/api/courses/route.ts
@@ -7,7 +7,7 @@ export async function POST(req: Request) {
   try {
     const { userId } = auth();
     const { title } = await req.json();
-    if (!userId || !isTeacher(userId)) {
+    if (!userId) {
       return new NextResponse("Unauthroized", { status: 401 });
     }
     const course = await db.course.create({
