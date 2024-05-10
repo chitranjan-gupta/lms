d81874ec41b5e8800d4a53989b4cd96f447c1447
Chitranjan Gupta <37315561+chitranjan-gupta@users.noreply.github.com>
2024-05-10 19:31:15 +0530

    Update layout.tsx
    removing isTeacher

--- lms/app/(dashboard)/(routes)/teacher/layout.tsx
+++ lms/app/(dashboard)/(routes)/teacher/layout.tsx
@@ -4,9 +4,6 @@ import { redirect } from "next/navigation";
 
 const TeacherLayout = ({ children }: { children: React.ReactNode }) => {
   const { userId } = auth();
-  if (!isTeacher(userId)) {
-    return redirect("/");
-  }
   return <>{children}</>;
 };
 
