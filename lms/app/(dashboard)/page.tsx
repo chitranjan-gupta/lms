c0f43288dbe1bd145bec069abba5e9f89ea4bbb7
Chitranjan Kumar Gupta <chitranjankumargupta123@gmail.com>
2024-05-21 07:00:30 +0000

    fix(web): vercel error

--- lms/app/(dashboard)/page.tsx
+++
@@ -1,9 +0,0 @@
-import { redirect } from "next/navigation";
-
-export default function Page() {
-  const userId = "";
-  if (!userId) {
-    return redirect("/sign-up");
-  }
-  return redirect("/dashboard");
-}
